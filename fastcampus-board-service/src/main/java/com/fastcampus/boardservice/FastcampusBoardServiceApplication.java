0299f7931b52393e83391c1117e5bd8db153884f
jaemin <jjm7655@gmail.com>
2024-04-04 16:03:51 +0900

    #4 - 스프링부트 프로젝트 시작 스프링부트 프로젝트 초기화 - 스프링 웹 - 액튜에이터 - 데브툴즈  - 롬복

---
+++ fastcampus-board-service/src/main/java/com/fastcampus/boardservice/FastcampusBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusBoardServiceApplication.class, args);
+    }
+
+}
