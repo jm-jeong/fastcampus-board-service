d43f48c7b0554e1e3ea92e3ad28a5bd8e6c0e499
jaemin <jjm7655@gmail.com>
2024-04-10 20:01:36 +0900

    #14   - 게시글 뷰 기본 골격 완성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- fastcampus-board-service/src/main/java/com/fastcampus/boardservice/config/ThymeleafConfig.java
+++ fastcampus-board-service/src/main/java/com/fastcampus/boardservice/config/ThymeleafConfig.java
@@ -3,7 +3,6 @@ package com.fastcampus.boardservice.config;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -23,7 +22,6 @@ public class ThymeleafConfig {
 
     @RequiredArgsConstructor
     @Getter
-    @ConstructorBinding
     @ConfigurationProperties("spring.thymeleaf3")
     public static class Thymeleaf3Properties {
         /**
